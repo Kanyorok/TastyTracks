194f2bc28174073e7be0f5967850fcc613143345
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2023-12-19 15:42:08 +0300

    Add the user information of the data

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
 
   resources :foods, only: %i[index show new create destroy]
   resources :general_shopping_lists, only: %i[index]
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
