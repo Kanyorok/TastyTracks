b3286de6e1396d5057727cffa45f4c6edafae5a2
Aung Pyae Khant <108471064+lilskyex0x@users.noreply.github.com>
2023-12-21 15:38:40 +0630

    Fix linter errors

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,40 +1,40 @@
 class FoodsController < ApplicationController
-    before_action :authenticate_user!
-  
-    def index
-      @foods = current_user.foods
-    end
+  before_action :authenticate_user!
 
-    def show
-        @food = current_user.foods.find(params[:id])
-    end
+  def index
+    @foods = current_user.foods
+  end
 
-    def new
-        @food = current_user.foods.build
-    end
-    
-    def create
-        @food = current_user.foods.build(food_params)
-        if @food.save
-          redirect_to foods_path, notice: 'Food item successfully added'
-        else
-          render :new
-        end
-    end
+  def show
+    @food = current_user.foods.find(params[:id])
+  end
 
-    def destroy
-        @food = current_user.foods.find(params[:id])
-        @food.recipe_foods.destroy_all
-        if @food.destroy
-          redirect_to foods_path, notice: 'Recipe item deleted successfully.'
-        else
-          redirect_to foods_path, alert: 'Failed to delete the Recipe Item!'
-        end
-    end
+  def new
+    @food = current_user.foods.build
+  end
 
-    private
-
-    def food_params
-        params.require(:food).permit(:name, :measurement_unit, :quantity, :price)
+  def create
+    @food = current_user.foods.build(food_params)
+    if @food.save
+      redirect_to foods_path, notice: 'Food item successfully added'
+    else
+      render :new
     end
+  end
+
+  def destroy
+    @food = current_user.foods.find(params[:id])
+    @food.recipe_foods.destroy_all
+    if @food.destroy
+      redirect_to foods_path, notice: 'Recipe item deleted successfully.'
+    else
+      redirect_to foods_path, alert: 'Failed to delete the Recipe Item!'
+    end
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :quantity, :price)
+  end
 end
