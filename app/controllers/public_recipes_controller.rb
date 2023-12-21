8911f99d5b791888cd52b5506e2ecf98ca9df194
Aung Pyae Khant <108471064+lilskyex0x@users.noreply.github.com>
2023-12-21 14:23:39 +0630

    Created show of public recipe controller

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -3,5 +3,9 @@ class PublicRecipesController < ApplicationController
     @public_recipes = Recipe.where(public: true).includes(recipe_foods: :food_name)
   end
 
-  def show; end
+  def show
+    @recipe = Recipe.includes(:user, :food_name).find(params[:id])
+    @user = @recipe.user
+    @foods = @recipe.food_name
+  end
 end
