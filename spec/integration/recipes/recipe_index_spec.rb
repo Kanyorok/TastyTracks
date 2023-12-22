6995eaba43e389137526836390b3f490fdd8744f
Aung Pyae Khant <108471064+lilskyex0x@users.noreply.github.com>
2023-12-23 00:51:08 +0630

    Expand test cases with additional tests

--- spec/integration/recipes/recipe_index_spec.rb
+++ spec/integration/recipes/recipe_index_spec.rb
@@ -16,29 +16,27 @@ RSpec.describe 'Recipe', type: :system do
   end
 
   context 'user logs in successfully' do
-    it 'should redirect to the root path' do
-      visit new_user_session_path
-      sleep(1)
-
-      fill_in 'Email', with: @user.email
-      fill_in 'Password', with: 'testing'
-
-      click_button 'Log in'
-      sleep(1)
+    it 'should redirect to the root path and display user recipes' do
+      login_user(@user)
       expect(current_path).to eq(root_path)
       expect(page).to have_content('Signed in successfully.')
+
+      expect(page).to have_content('My Recipes')
+      sleep(1)
+      click_link @r1.name
+      sleep(1)
+      expect(current_path).to eq public_recipe_path(@r1)
+      visit root_path
+      sleep(1)
+      click_link @r3.name
+      sleep(1)
+      expect(current_path).to eq public_recipe_path(@r3)
     end
   end
 
   context '#index' do
     it 'should direct to recipes index' do
-      visit new_user_session_path
-      sleep(1)
-
-      fill_in 'Email', with: @user.email
-      fill_in 'Password', with: 'testing'
-
-      click_button 'Log in'
+      login_user(@user)
       sleep(1)
       visit recipes_path
       sleep(1)
