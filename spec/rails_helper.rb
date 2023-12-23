c6233791b67f9546a657c7bbddc6b8e284b2ff62
Aung Pyae Khant <108471064+lilskyex0x@users.noreply.github.com>
2023-12-23 17:34:00 +0630

    Added configuration for controller helper in rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,6 +34,7 @@ end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.include LoginHelper, type: :system
+  config.include Devise::Test::ControllerHelpers, type: :controller
   config.fixture_path = "#{Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
