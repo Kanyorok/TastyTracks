e69547bed41467597f1ec7359ddb4b4d363e2136
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2023-12-23 18:19:12 +0300

    Fix the recipefood controller tests

--- spec/controller/recipe_foods_controller_spec.rb
+++ spec/controller/recipe_foods_controller_spec.rb
@@ -7,13 +7,22 @@ RSpec.describe RecipeFoodsController, type: :controller do
     sign_in @user
     @recipe = @user.recipes.create!(name: 'Test Recipe', preparation_time: 30, cooking_time: 45)
     @food = @user.foods.create!(name: 'Test Food', measurement_unit: 'kg', quantity: 1, price: 1)
-    @recipe_food = @recipe.recipe_foods.create!(recipe_name: @recipe, food_name: @food, quantity: 1)
+    @recipe_food = @recipe.recipe_foods.create!(food_name: @food, quantity: 1)
   end
 
   describe 'GET #new' do
     it 'assigns a new RecipeFood to @recipe_food' do
       get :new, params: { recipe_id: @recipe.id }
       expect(assigns(:recipe_food)).to be_a_new(RecipeFood)
+    end
+  end
+
+  describe 'POST #create' do
+    it 'creates a new RecipeFood' do
+      expect do
+        post :create, params: { recipe_id: @recipe.id, recipe_food: { food_id: @food.id, quantity: 2 } }
+      end.to change(RecipeFood, :count).by(1)
+      expect(response).to redirect_to(recipe_path(@recipe))
     end
   end
 
@@ -29,6 +38,7 @@ RSpec.describe RecipeFoodsController, type: :controller do
       patch :update, params: { recipe_id: @recipe.id, id: @recipe_food.id, recipe_food: { quantity: 3 } }
       @recipe_food.reload
       expect(@recipe_food.quantity).to eq(3)
+      expect(response).to redirect_to(recipe_path(@recipe))
     end
   end
 
@@ -37,6 +47,7 @@ RSpec.describe RecipeFoodsController, type: :controller do
       expect do
         delete :destroy, params: { recipe_id: @recipe.id, id: @recipe_food.id }
       end.to change(RecipeFood, :count).by(-1)
+      expect(response).to redirect_to(recipe_path(@recipe))
     end
   end
 end
